b9e3ec26a687f68827cf3bdfb90d9365dee321b4
Yasser Souri <yassersouri@gmail.com>
2011-03-10 17:07:42 +0330

    add the function that gets the mehrnews index but there is problem with the encoding i guess

--- src/PersianLetter.java
+++ src/PersianLetter.java
@@ -6,14 +6,25 @@
  *
  */
 
+import helper.InputFiller;
+
 import java.io.*;
 import java.util.*;
+import java.net.*;
+import org.htmlcleaner.*;
 
 public class PersianLetter {
 	
 	
 	public static void main(String args[]){
+		InputFiller inputfiller = new InputFiller();
 		
+
+		try {
+			inputfiller.getMehrnewsPage();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
--- src/helper/InputFiller.java
+++ src/helper/InputFiller.java
@@ -3,17 +3,34 @@ package helper;
 import java.io.*;
 import java.util.*;
 import java.net.*;
+
+
 import org.htmlcleaner.*;
 
 public class InputFiller {
 	
 	String mehrnewsURL = "http://www.mehrnews.ir/txtNewsView_fa.aspx?t=News&Page=1";
-	String mehrnewsSavePath = "pages/mehrnews.html";
+	String mehrnewsSavePath = "mehrnews.html";
 	
-	public InputFiller getMehrnewsPage(){
+	public InputFiller getMehrnewsPage() throws IOException{
 		
+		URL mehrnews = new URL(mehrnewsURL);
+		URLConnection mehrnewsConnection = mehrnews.openConnection();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(mehrnewsConnection.getInputStream()));
 		
+		FileWriter writer = new FileWriter(new File(mehrnewsSavePath));
+
+		String htmlLine;
+		while((htmlLine = reader.readLine()) != null){
+			writer.append(htmlLine);
+		}
+		
+		writer.flush();
+		writer.close();
+		reader.close();
 		
 		return this;
 	}
+	
+
 }
