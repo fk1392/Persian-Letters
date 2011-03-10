b0ffe705241fab4f2e57f790ca114f10e1ebda9e
Yasser Souri <yassersouri@gmail.com>
2011-03-10 16:05:52 +0330

    initial commit

---
+++ src/PersianLetter.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Yasser
+ * 
+ * This Class reads the input.txt file and fills the output.txt file
+ *
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class PersianLetter {
+	
+	
+	public static void main(String args[]){
+		
+	}
+	
+	
+}
---
+++ src/helper/InputFiller.java
@@ -0,0 +1,19 @@
+package helper;
+
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import org.htmlcleaner.*;
+
+public class InputFiller {
+	
+	String mehrnewsURL = "http://www.mehrnews.ir/txtNewsView_fa.aspx?t=News&Page=1";
+	String mehrnewsSavePath = "pages/mehrnews.html";
+	
+	public InputFiller getMehrnewsPage(){
+		
+		
+		
+		return this;
+	}
+}
